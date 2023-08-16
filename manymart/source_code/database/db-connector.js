55275daed6af718bcdae10e8bab2fc7385c56435
Riley Ovenshire <87671333+rileyovenshire@users.noreply.github.com>
2023-08-16 11:41:00 -0500

    Add files via upload

--- manymart/source_code/database/db-connector.js
+++ manymart/source_code/database/db-connector.js
@@ -1,1 +1,13 @@
+var mysql = require('mysql')
 
+// Create a 'connection pool' using the provided credentials
+var pool = mysql.createPool({
+    connectionLimit : 10,
+    host            : 'classmysql.engr.oregonstate.edu',
+    user            : 'cs340_ovenshir',
+    password        : 'wVkagwGRBkRU',
+    database        : 'cs340_ovenshir'
+})
+
+// Export it for use in our application
+module.exports.pool = pool;
