2a0f8dcdb747c8124194ce195fae31a2a2daf11f
Riley Ovenshire <87671333+rileyovenshire@users.noreply.github.com>
2023-08-16 11:46:12 -0500

    Update db-connector.js

--- manymart/source_code/database/db-connector.js
+++ manymart/source_code/database/db-connector.js
@@ -3,10 +3,10 @@ var mysql = require('mysql')
 // Create a 'connection pool' using the provided credentials
 var pool = mysql.createPool({
     connectionLimit : 10,
-    host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_ovenshir',
-    password        : 'wVkagwGRBkRU',
-    database        : 'cs340_ovenshir'
+    host            : '',
+    user            : '',
+    password        : '',
+    database        : ''
 })
 
 // Export it for use in our application
