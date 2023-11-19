66b16daeb89037b690dd22b05c9402b369285bef
Riley Ovenshire <87671333+rileyovenshire@users.noreply.github.com>
2023-11-19 08:56:55 -0600

    Update stack-or-queue.py
    Formatting.

--- lessons/stack-or-queue.py
+++ lessons/stack-or-queue.py
@@ -1,128 +1,120 @@
-#                                     Stack or Queue?
+# Stack or Queue?
 
+print("\nBROWSER HISTORY -----------------------------------------------------------------------------------------------")
 # Challenge 1: Browser Back Button
-# Scenario: You're writing a simple web browser. Implement a feature to keep track of the pages visited, so that when the 'Back' button is pressed, the browser goes back to the previously visited page.
-
-# Hint: Think about the order in which web pages are visited and how the 'Back' button should work. Does it follow a Last-In-First-Out (LIFO) or a First-In-First-Out (FIFO) approach?
-
 class BrowserHistory:
     def __init__(self):
-        # Initialize your data structure here
-        # TODO: Implement this
-
-    def visit(self, page):
-        # User visits a page
-        # TODO: Implement this
-
-    def back(self):
-        # User presses the back button
-        # TODO: Implement this
+        """ Initialize your data structure here """
         pass
 
-# Test the BrowserHistory class
+    def visit(self, page):
+        """ User visits a page """
+        pass
+
+    def back(self):
+        """ User presses the back button """
+        pass
+
 def test_browser_history():
-    # TODO: Create a BrowserHistory instance and test visiting pages and going back
+    browser = BrowserHistory()
+    browser.visit("google.com")
+    browser.visit("facebook.com")
+    browser.visit("leetcode.com")
+    print(browser.back())  # Should return the last visited page
+    print(browser.back())  # Should return the second last visited page
+    print(browser.back())  # Should return the first visited page
 
 test_browser_history()
 
-#----------------------------------------------------------------------------------------------
-
+print("\nPRINT QUEUE -----------------------------------------------------------------------------------------------")
 # Challenge 2: Printer Queue
-# Scenario: Create a system for a printer to manage print jobs. Each new print request should be added to the printer's list of jobs, and jobs should be printed in the order they were received.
-
-# Hint: Consider how a printer processes print jobs. Should it print the most recently added job first or the first job that was added?
-
 class PrinterQueue:
     def __init__(self):
-        # Initialize your printer queue here
-        # TODO: Implement this
-
-    def add_job(self, job):
-        # Add a print job to the queue
-        # TODO: Implement this
-
-    def print_job(self):
-        # Print the next job in the queue
-        # TODO: Implement this
+        """ Initialize your printer queue here """
         pass
 
-# Test the PrinterQueue class
+    def add_job(self, job):
+        """ Add a print job to the queue """
+        pass
+
+    def print_job(self):
+        """ Print the next job in the queue """
+        pass
+
 def test_printer_queue():
-    # TODO: Create a PrinterQueue instance and test adding and printing jobs
+    printer = PrinterQueue()
+    printer.add_job("Job 1")
+    printer.add_job("Job 2")
+    printer.add_job("Job 3")
+    print(printer.print_job())  # Should print "Job 1"
+    print(printer.print_job())  # Should print "Job 2"
+    print(printer.print_job())  # Should print "Job 3"
 
 test_printer_queue()
 
-
-#----------------------------------------------------------------------------------------------
-
+print("\nTEXT EDITOR -----------------------------------------------------------------------------------------------")
 # Challenge 3: Undo Functionality in a Text Editor
-# Scenario: Implement an 'Undo' feature in a text editor. This feature should allow users to undo their last action, and each subsequent undo should reverse the previous action in reverse order.
-
-# Hint: Reflect on how the undo functionality should work. When a user undoes an action, should it be the most recent one or the oldest one?
-
 class TextEditor:
     def __init__(self):
-        # Initialize your data structure here
-        # TODO: Implement this
-
-    def write(self, text):
-        # User writes text
-        # TODO: Implement this
-
-    def undo(self):
-        # Undo the last write action
-        # TODO: Implement this
+        """ Initialize your data structure here """
         pass
 
-# Test the TextEditor class
+    def write(self, text):
+        """ User writes text """
+        pass
+
+    def undo(self):
+        """ Undo the last write action """
+        pass
+
 def test_text_editor():
-    # TODO: Create a TextEditor instance and test writing and undoing
+    editor = TextEditor()
+    editor.write("Hello")
+    editor.write(" World")
+    editor.write("!")
+    print(editor.undo())  # Should undo the last write action
+    print(editor.undo())  # Should undo the second last write action
+    print(editor.undo())  # Should undo the first write action
 
 test_text_editor()
 
-
-#----------------------------------------------------------------------------------------------
-
+print("\nCUSTOMER SERVICE -----------------------------------------------------------------------------------------------")
 # Challenge 4: Customer Service Line
-# Scenario: Design a system to manage a customer service line. Customers call and are put on hold until a representative is available. The system should ensure that the customers are served in the order they called.
-
-# Hint: Think about the order in which customers should be served. Is it fair to serve the last caller first, or should it be the first caller?
-
 class CustomerServiceLine:
     def __init__(self):
-        # Initialize your data structure here
-        # TODO: Implement this
-
-    def call_received(self, customer):
-        # A new customer call is received
-        # TODO: Implement this
-
-    def serve_customer(self):
-        # Serve the next customer
-        # TODO: Implement this
+        """ Initialize your data structure here """
         pass
 
-# Test the CustomerServiceLine class
+    def call_received(self, customer):
+        """ A new customer call is received """
+        pass
+
+    def serve_customer(self):
+        """ Serve the next customer """
+        pass
+
 def test_customer_service():
-    # TODO: Create a CustomerServiceLine instance and test receiving and serving calls
+    customer_service = CustomerServiceLine()
+    customer_service.call_received("Customer 1")
+    customer_service.call_received("Customer 2")
+    customer_service.call_received("Customer 3")
+    print(customer_service.serve_customer())  # Should serve "Customer 1"
+    print(customer_service.serve_customer())  # Should serve "Customer 2"
+    print(customer_service.serve_customer())  # Should serve "Customer 3"
 
 test_customer_service()
 
-
-#----------------------------------------------------------------------------------------------
-
+print("\nBALANCING PARENTHESES -----------------------------------------------------------------------------------------------")
 # Challenge 5: Balancing Parentheses
-# Scenario: Write a program that checks if a string of parentheses is balanced. Every opening parenthesis should have a corresponding closing parenthesis in the correct order.
-
-# Hint: Analyze how parentheses open and close. When checking for balance, do you need to consider the most recent parenthesis first, or the first parenthesis that was opened?
-
 def is_balanced(parentheses):
-    # Check if the parentheses string is balanced
-    # TODO: Implement this function
+    """ Check if the parentheses string is balanced """
     pass
 
-# Test the is_balanced function
 def test_balancing_parentheses():
-    # TODO: Test the function with different parentheses strings
+    print(is_balanced("()()"))  # Should return True
+    print(is_balanced("(()"))  # Should return False
+    print(is_balanced("())"))  # Should return False
+    print(is_balanced("((()))"))  # Should return True
+    print(is_balanced("((())"))  # Should return False
 
 test_balancing_parentheses()
